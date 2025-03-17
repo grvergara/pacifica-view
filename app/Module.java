b922aa171c20b5b98d6ae80fe9a2cddd074e0864
Gonzalo Vergara <grvergara@gmail.com>
2025-03-17 10:46:59 -0300

    add bindings for DI (akkaGuiceSupport)

--- app/Module.java
+++ app/Module.java
@@ -1,13 +1,16 @@
+import actors.UserParentActor;
 import com.google.inject.AbstractModule;
+import play.libs.akka.AkkaGuiceSupport;
 
 import java.time.Clock;
 
 import static java.time.Clock.systemUTC;
 
-public class Module extends AbstractModule {
+public class Module extends AbstractModule implements AkkaGuiceSupport {
 
     @Override
     protected void configure() {
         bind(Clock.class).toInstance(systemUTC());
+        bindActor(UserParentActor.class, "userParentActor"); //Important!! To implement DI
     }
 }
